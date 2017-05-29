08cd7695dccec4f01fe3a7b2c2a1ddcdf1a8873d
jeonjiwon <jwurbane97@ajou.ac.kr>
2017-05-29 10:10:03 +0900

    testz

--- src/kr/ac/ajou/oop/managers/FileManager.java
+++ src/kr/ac/ajou/oop/managers/FileManager.java
@@ -1,9 +1,11 @@
 package kr.ac.ajou.oop.managers;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -13,41 +15,46 @@ import kr.ac.ajou.oop.user.User;
 public final class FileManager implements Serializable {
 
 	private FileManager() {}
-//  To prevent creating an object
-//   1. Make constructor private
-//   2. Make class abstract
+	// To prevent creating an object
+	// 1. Make constructor private
+	// 2. Make class abstract
 
-    public static void saveUser(User u) throws IOException {
-        FileOutputStream fos = new FileOutputStream("user.dat");
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
+	public static void saveUser(User u) throws IOException {
+		FileOutputStream fos = new FileOutputStream("user.dat");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-        oos.writeObject(u);
-        oos.close();
-    }
+		oos.writeObject(u);
+		oos.close();
+	}
 
-    public static String loadGuidance(int level) throws IOException, ClassNotFoundException {
-        String hint;
+	public static String loadGuidance(int level) throws IOException, ClassNotFoundException {
+		String hint = "";
+		int c;
+		FileInputStream fis = new FileInputStream("guide" + level + ".txt");
 
-        FileInputStream fis = new FileInputStream("guide"+level+".dat");
-        ObjectInputStream ois = new ObjectInputStream(fis);
+		while ((c = fis.read()) != -1)
+			hint += (char) fis.read();
 
-        hint = (String)ois.readObject();
-        ois.close();
+		fis.close();
+		return hint;
+	}
 
-        return hint;
+	public static String loadAnswerCode(int level) throws FileNotFoundException, IOException {
+		String code;
 
-    }
+		BufferedReader br = new BufferedReader(new FileReader("answer" + level + ".txt"));
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
 
-    public static String loadAnswerCode(int level) throws IOException, ClassNotFoundException {
-        String code;
-
-        FileInputStream fis = new FileInputStream("answer"+level+".dat");
-        ObjectInputStream ois = new ObjectInputStream(fis);
-
-        code = (String)ois.readObject();
-        ois.close();
-
-        return code;
-    }
+		while (line != null) {
+			sb.append(line);
+			sb.append(System.lineSeparator());
+			line = br.readLine();
+		}
+		
+		code = sb.toString();
+		br.close();
+		return code;
+	}
 
 }
