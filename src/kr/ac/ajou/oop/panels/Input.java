246308dd2bb1b598ef6c0bbe5fd7c129622c5ce5
KiaraR <hcho2256@naver.com>
2017-06-13 03:22:56 +0900

    code 자동수정을 해보려했다

--- src/kr/ac/ajou/oop/panels/Input.java
+++ src/kr/ac/ajou/oop/panels/Input.java
@@ -38,7 +38,7 @@ public class Input extends JPanel implements ActionListener {
 
 		answers = new JPanel();
 		answers.setLayout(new BoxLayout(answers, BoxLayout.PAGE_AXIS));
-		
+
 		JScrollPane scrollPane = new JScrollPane(answers);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
@@ -48,45 +48,48 @@ public class Input extends JPanel implements ActionListener {
 	public void setComponents() {
 		answers.removeAll();
 		repaint();
-		
+
 		btnCheckMyAnswer = new JButton("Check my Answer");
 		btnCheckMyAnswer.addActionListener(this);
-		
+
 		answers.add(btnCheckMyAnswer);
-		
+
 		int elements = FileManager.getTFAmount(g.getUser().getLevel());
-		
+
 		inputs = new JLabel[elements];
 		tfAnswer = new JTextField[elements];
 		ishas = new JComboBox[elements];
-		
+
 		for (int i = 0; i < elements; i++) {
 			inputs[i] = new JLabel("Answer" + (i + 1) + ":");
 			answers.add(inputs[i]);
 			if (g.getUser().getLevel() == 2) {
-				ishas[i] = new JComboBox<String>(new String[]{"is", "has"});
+				ishas[i] = new JComboBox<String>(new String[] { "is", "has" });
 				answers.add(ishas[i]);
 			} else {
 				tfAnswer[i] = new JTextField(10);
+				tfAnswer[i].addActionListener(this);
 				answers.add(tfAnswer[i]);
 			}
 		}
-		
+
 	}
 
 	public boolean compare(int level) {
 		try {
 			arr = FileManager.answers(level);
-			
+
 			if (g.getUser().getLevel() == 2) {
 				for (int i = 0; i < arr.size(); i++) {
-					if (!ishas[i].getSelectedItem().equals(arr.get(i))) return false;
-						
+					if (!ishas[i].getSelectedItem().equals(arr.get(i)))
+						return false;
+
 				}
 			} else {
 				for (int i = 0; i < arr.size(); i++) {
-					if (!tfAnswer[i].getText().equals(arr.get(i))) return false;
-						
+					if (!tfAnswer[i].getText().equals(arr.get(i)))
+						return false;
+
 				}
 			}
 
@@ -98,12 +101,17 @@ public class Input extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (g.getUser().getLevel() == 1 || compare(g.getUser().getLevel())) {
-			g.setID(State.STATE_ANSWER_CORRECT);
-		} else {
-			g.setID(State.STATE_ANSWER_INCORRECT);
+		if (e.getSource() == btnCheckMyAnswer) {
+			if (g.getUser().getLevel() == 1 || compare(g.getUser().getLevel())) {
+				g.setID(State.STATE_ANSWER_CORRECT);
+			} else {
+				g.setID(State.STATE_ANSWER_INCORRECT);
+			}
+			g.update();
 		}
-		g.update();
+		else if(e.getSource() == tfAnswer[0]) {
+			//System.out.println("aa");
+		}
 	}
 
 }
