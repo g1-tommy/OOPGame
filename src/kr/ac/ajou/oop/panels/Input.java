9bbc35061cf4c762ddedcb74bd3efacf2e368caf
KiaraR <hcho2256@naver.com>
2017-06-12 10:49:57 +0900

    err fix

--- src/kr/ac/ajou/oop/panels/Input.java
+++ src/kr/ac/ajou/oop/panels/Input.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -25,142 +26,138 @@ public class Input extends JPanel implements ActionListener {
 
 	private JButton btnCheckMyAnswer;
 	private Game g;
-	
-    public Input(Game g) {
-    	this.g = g;
-    	
-    	setBorder(new TitledBorder(null, "Input", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-    	
-    	btnCheckMyAnswer = new JButton("Check my Answer");
+
+	public Input(Game g) {
+		this.g = g;
+
+		setBorder(new TitledBorder(null, "Input", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+
+		btnCheckMyAnswer = new JButton("Check my Answer");
 		btnCheckMyAnswer.addActionListener(this);
-		
+
 		GroupLayout gl_input = new GroupLayout(this);
-		gl_input.setHorizontalGroup(gl_input.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING, gl_input.createSequentialGroup().addContainerGap(287, Short.MAX_VALUE).addComponent(btnCheckMyAnswer)));
-		gl_input.setVerticalGroup(gl_input.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING, gl_input.createSequentialGroup().addContainerGap(110, Short.MAX_VALUE).addComponent(btnCheckMyAnswer)));
-		
-    	setLayout(gl_input);
-    }
-    
-    public boolean compare(int level) throws IOException {	
+		gl_input.setHorizontalGroup(gl_input.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+				gl_input.createSequentialGroup().addContainerGap(287, Short.MAX_VALUE).addComponent(btnCheckMyAnswer)));
+		gl_input.setVerticalGroup(gl_input.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+				gl_input.createSequentialGroup().addContainerGap(110, Short.MAX_VALUE).addComponent(btnCheckMyAnswer)));
 
-    	if(level == 1){
-    		
-        	FileReader fReader = new FileReader("text.txt"); // level 1ÀÇ answer txt
-            BufferedReader br = new BufferedReader(fReader);
-            
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    		
-    		
-            String s;
-            while((s = br.readLine()) != null){
-    		
-    		if(answer_1.getText()==s && answer_2.getText() == s){
-    			return true;
-    		}
-    		else
-    			return false;
-            }
-            
-      	}
+		setLayout(gl_input);
+	}
 
-    	
-    	else if(level == 2){
-    		
-    		JTextArea textArea = new JTextArea(5, 30);
-    	
-    		JScrollPane scrollPane = new JScrollPane();
-    		scrollPane.add(textArea);
-    		
-    		setPreferredSize(new Dimension(450, 110));
-    		
-    	}
-    	
-    	else if(level ==3){
-    		
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    		
-    	}
-    	
-    	
-    	else if(level ==3){
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    	}
-    	
-    	
-    	else if(level ==4){
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    		
-    	}
-    	
-    	
-    	else if(level==5){
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    	}
-    	
-    	
-    	else if(level==6){
-    		JLabel lblAnswer = new JLabel("Answer1:");
-    		JTextField answer_1 = new JTextField();
-    		answer_1.setColumns(10);
-    		
-    		
-    		JLabel lblAnswer_1 = new JLabel("Answer2:");
-    		JTextField answer_2 = new JTextField();
-    		answer_2.setColumns(10);
-    	}
-    	
-    	
-    	return false;
-    }
-    	
-    
+	public boolean compare(int level) {
+
+		if (level == 1) {
+
+			FileReader fReader;
+			BufferedReader br;
+			try {
+				fReader = new FileReader("text.txt");
+				br = new BufferedReader(fReader);
+
+				JLabel lblAnswer = new JLabel("Answer1:");
+				JTextField answer_1 = new JTextField();
+				answer_1.setColumns(10);
+
+				JLabel lblAnswer_1 = new JLabel("Answer2:");
+				JTextField answer_2 = new JTextField();
+				answer_2.setColumns(10);
+
+				String s;
+				while ((s = br.readLine()) != null) {
+
+					if (answer_1.getText() == s && answer_2.getText() == s) {
+						return true;
+					} else
+						return false;
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} // level 1ÀÇ answer txt
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		else if (level == 2) {
+
+			JTextArea textArea = new JTextArea(5, 30);
+
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.add(textArea);
+
+			setPreferredSize(new Dimension(450, 110));
+
+		}
+
+		else if (level == 3) {
+
+			JLabel lblAnswer = new JLabel("Answer1:");
+			JTextField answer_1 = new JTextField();
+			answer_1.setColumns(10);
+
+			JLabel lblAnswer_1 = new JLabel("Answer2:");
+			JTextField answer_2 = new JTextField();
+			answer_2.setColumns(10);
+
+		}
+
+		else if (level == 3) {
+			JLabel lblAnswer = new JLabel("Answer1:");
+			JTextField answer_1 = new JTextField();
+			answer_1.setColumns(10);
+
+			JLabel lblAnswer_1 = new JLabel("Answer2:");
+			JTextField answer_2 = new JTextField();
+			answer_2.setColumns(10);
+		}
+
+		else if (level == 4) {
+			JLabel lblAnswer = new JLabel("Answer1:");
+			JTextField answer_1 = new JTextField();
+			answer_1.setColumns(10);
+
+			JLabel lblAnswer_1 = new JLabel("Answer2:");
+			JTextField answer_2 = new JTextField();
+			answer_2.setColumns(10);
+
+		}
+
+		else if (level == 5) {
+			JLabel lblAnswer = new JLabel("Answer1:");
+			JTextField answer_1 = new JTextField();
+			answer_1.setColumns(10);
+
+			JLabel lblAnswer_1 = new JLabel("Answer2:");
+			JTextField answer_2 = new JTextField();
+			answer_2.setColumns(10);
+		}
+
+		else if (level == 6) {
+			JLabel lblAnswer = new JLabel("Answer1:");
+			JTextField answer_1 = new JTextField();
+			answer_1.setColumns(10);
+
+			JLabel lblAnswer_1 = new JLabel("Answer2:");
+			JTextField answer_2 = new JTextField();
+			answer_2.setColumns(10);
+		}
+
+		return false;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(compare(g.getUser().getLevel())==true) {
-				g.setID(State.STATE_ANSWER_CORRECT);
-				g.update();
-		}else{
+		if (compare(g.getUser().getLevel()) == true) {
+			g.setID(State.STATE_ANSWER_CORRECT);
+			g.update();
+		} else {
 			g.setID(State.STATE_ANSWER_INCORRECT);
 
-			
 			g.update();
 		}
 	}
-    
+
 }
